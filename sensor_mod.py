5af86d57cf8093ff9e7967f1e385718ae166ccba
caryben <bencary@mit.edu>
2016-07-25 20:34:12 -0700

    Update sensor_mod.py
    lenny face can't be in the code for python 2.7 to work lmao

--- sensor_mod.py
+++ sensor_mod.py
@@ -1,4 +1,4 @@
-# uses ~0.0% of the processor and 0.1% of memory so it can run silently in the background ( ͡° ͜ʖ ͡°)
+# uses ~0.0% of the processor and 0.1% of memory so it can run silently in the background
 import subprocess, time
 subprocess.check_call(['echo', '-ne', '\e[8;15;60t'])
 while(True):
